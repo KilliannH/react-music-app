6bf464cbb4a6634b7f3e80ba1a25f79df5e22ddc
KilliannH <killiannhervagault@gmail.com>
2019-05-24 14:11:03 +0200

    class component exemple

--- src/App.js
+++ src/App.js
@@ -1,17 +1,43 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 import Person from './Person/Person';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Hi, I'm a react app!</h1>
-      <p>This is really working !</p>
-      <Person name="Max" age="28"/>
-        <Person name="Manu" age="32"/>
-        <Person name="Stefany" age="30"/>
-    </div>
-  );
+class App extends Component {
+
+    state = {
+        persons:
+            [
+                {name: 'Max', age: 28},
+                {name: 'Manu', age: 32},
+                {name: 'Stephany', age: 30}
+            ]
+    };
+
+    switchStateHandler = () => {
+        // console.log('Was clicked');
+        // DONT DO THIS : this.state.persons[0].name = 'Maximilian';
+        this.setState({
+            persons:
+                [
+                    {name: 'Maximilian', age: 28},
+                    {name: 'Manu', age: 32},
+                    {name: 'Stephany', age: 27}
+                ]
+        })
+    };
+
+  render() {
+      return (
+          <div className="App">
+              <h1>Hi, I'm a react app!</h1>
+              <p>This is really working !</p>
+              <button onClick={this.switchStateHandler}>Switch Name</button>
+              <Person name={this.state.persons[0].name} age={this.state.persons[0].age}>Coucou</Person>
+              <Person name={this.state.persons[1].name} age={this.state.persons[1].age}>Salut</Person>
+              <Person name={this.state.persons[2].name} age={this.state.persons[2].age}>Bonjour</Person>
+          </div>
+      );
+  }
 }
 
 export default App;
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,7 +1,12 @@
 import React from 'react';
 
 const person = (props) => {
-    return <p>I am a {props.name} and i am {props.age} years old.</p>
+    return (
+        <div>
+            <p>I am a {props.name} and i am {props.age} years old.</p>
+            <p>{props.children}</p>
+        </div>
+    )
 };
 
 
